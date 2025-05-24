772376e41da40d8e48353b67f48f5bbc682d6143
ThamizhiniyanCS <csthamizhiniyan@gmail.com>
2025-05-24 15:11:10 +0530

    Added Support for Tailwind Variants

--- .config/nvim/lua/plugins/extend-nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/extend-nvim-lspconfig.lua
@@ -4,5 +4,20 @@ return {
   opts = {
     ---@type lspconfig.options
     servers = {},
+    -- Ref: https://www.lazyvim.org/extras/lang/tailwind#nvim-lspconfig
+    setup = {
+      tailwindcss = function(_, opts)
+        opts.settings = {
+          -- Tailwind Variants IntelliSense Setup: https://www.tailwind-variants.org/docs/getting-started#intellisense-setup-optional
+          tailwindCSS = {
+            experimental = {
+              classRegex = {
+                { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
+              },
+            },
+          },
+        }
+      end,
+    },
   },
 }
